a44daf65e367faa2de7e341c4c26daec646489e4
Rady <radyibrahim777@gmail.com>
2026-06-19 18:58:12 +0300

    Doctor Routes

--- Modules/Doctor/Providers/DoctorServiceProvider.php
+++ Modules/Doctor/Providers/DoctorServiceProvider.php
@@ -13,7 +13,7 @@ class DoctorServiceProvider extends ServiceProvider
     {
         // Routes are registered via routes/api.php and routes/dashboard.php
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->loadRoutesFrom(base_path('Modules/doctor/Routes/doctor.php'));
+        $this->loadRoutesFrom(base_path('Modules/Doctor/Routes/doctor.php'));
 
     }
 }
